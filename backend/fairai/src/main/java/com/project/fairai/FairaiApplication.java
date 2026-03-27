77dd05212b34f8aa75283b8f0c38e0022d59bb5e
Himesh <himeshkumar562@gmail.com>
2026-03-27 15:06:17 +0530

    Added backend skeleton

---
+++ backend/fairai/src/main/java/com/project/fairai/FairaiApplication.java
@@ -0,0 +1,13 @@
+package com.project.fairai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairaiApplication.class, args);
+    }
+
+}
